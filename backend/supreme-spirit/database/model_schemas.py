d7828748d684dad89a022fdf8b7ceb27cf811a07
Vasiliy Kovalev <minskij10@yandex.ru>
2019-12-07 02:57:20 +0300

    Add get

--- backend/supreme-spirit/database/model_schemas.py
+++ backend/supreme-spirit/database/model_schemas.py
@@ -1,5 +1,6 @@
+from typing import Dict
 from pydantic import BaseModel
-from database.models.models import AccountType
+from database.models.models import AccountType, OfferType
 import datetime
 
 class UserBase(BaseModel):
@@ -35,6 +36,7 @@ class AccountModel(AccountBase):
     class Config:
         orm_mode = True
 
+
 class TransactionBase(BaseModel):
     from_user : int
     to_user : int
@@ -42,8 +44,10 @@ class TransactionBase(BaseModel):
     to_account : int
     amount : float
 
+
 class TransactionCreate(TransactionBase):
     pass
+
 
 class TransactionModel(TransactionBase):
     id: int
@@ -56,3 +60,55 @@ class TransactionModel(TransactionBase):
     class Config:
         orm_mode = True
 
+
+class OfferTemplateBase(BaseModel):
+    type : OfferType
+    text : str
+    data : Dict
+
+
+class OfferTemplateCreate(OfferTemplateBase):
+    pass
+
+
+class OfferTemplateModel(OfferTemplateBase):
+    id: int
+    created_at : datetime.datetime
+
+    class Config:
+        orm_mode = True
+
+
+class OfferFilterBase(BaseModel):
+    filter : Dict
+
+
+class OfferFilterCreate(OfferFilterBase):
+    pass
+
+
+class OfferFilterModel(OfferFilterBase):
+    id: int
+    created_at : datetime.datetime
+
+    class Config:
+        orm_mode = True
+
+
+class BoundOfferTemplateBase(BaseModel):
+    filter : Dict
+    offer_filter_id : int
+    offer_template_id : int
+
+
+class BoundOfferTemplateCreate(BoundOfferTemplateBase):
+    offer_filter : OfferFilterModel
+    offer_template : OfferTemplateModel
+
+
+class BoundOfferTemplateModel(BoundOfferTemplateBase):
+    id: int
+    created_at : datetime.datetime
+
+    class Config:
+        orm_mode = True
